88184d92ae99a7820cdb5f2145010c8ce4d541d2
ilyataster <taster00@gmail.com>
2021-07-11 12:30:32 +0400

    allvoices page fix

--- src/allVoices.js
+++ src/allVoices.js
@@ -1,7 +1,9 @@
 export async function allVoicesActivate() {
     const response = await fetch('https://voicy-speaker.herokuapp.com/voices')
     const data = await response.json()
-    for (let i = data.length - 5; i < data.length; i++) {
+    let i = data.length >= 5 ? data.length - 5 : 0
+
+    for (i; i < data.length; i++) {
         //getting audio messages and creating audio file
         const audio = document.createElement('audio');
         audio.controls = true
@@ -17,21 +19,5 @@ export async function allVoicesActivate() {
         li.innerHTML = data[i].timeStamp.substr(0, 24);
         li.appendChild(audio)
         document.getElementById('allVoicesList').appendChild(li);
-    }
-
-    for (let i = data.length - 5; i < data.length; i++) {
-        //attaching audio file to list object
-        const li = document.createElement('li');
-        li.classList.add('audioElement');
-        li.classList.add('voice_message');
-        li.innerHTML = data[i].timeStamp.substr(0, 24);
-        li.addEventListener('click', function(){
-            const audioBlob = new Blob([new
-            Uint8Array(data[i].audioBlob[0].data).buffer]);
-            const audioUrl = URL.createObjectURL(audioBlob);
-            const audio = new Audio(audioUrl)
-            audio.play()
-        })
-        document.getElementById('voiceList').appendChild(li);
     }
 }
